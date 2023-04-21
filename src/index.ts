05ca5fd81aff1beaf2d0cba282b35b86db472731
krsbx <firdaus.sati51@gmail.cm>
2023-04-21 14:30:17 +0700

    chore: initialize repo

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { config as dotenvConfig } from 'dotenv';
+import express from 'express';
+import db from './models';
+
+dotenvConfig();
+
+const PORT = +(process.env.PORT ?? 3001);
+
+db.sequelize.sync();
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Sever are running @ ${PORT}`);
+});
