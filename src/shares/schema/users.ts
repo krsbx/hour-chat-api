95796be01d06b9828c36b7af662031117ac233ca
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2023-06-04 19:31:04 +0700

    chore: update user validation schema

--- src/shares/schema/users.ts
+++ src/shares/schema/users.ts
@@ -1,18 +1,26 @@
 import { z } from 'zod';
+import moment from 'moment';
 import validator from 'validator';
 import { GENDER } from '../constant';
 
 export const createUserSchema = z
   .object({
     firstName: z.string().min(3),
-    middleName: z.string().min(3).optional(),
-    lastName: z.string().min(3).optional(),
+    middleName: z.string().optional(),
+    lastName: z.string().optional(),
     username: z.string().min(5),
     email: z.string().email(),
     phoneNumber: z.string().refine(validator.isMobilePhone),
     password: z.string().min(5),
     confirmPassword: z.string().min(5),
-    dob: z.date().optional().nullable().default(null),
+    dob: z
+      .string()
+      .optional()
+      .refine((date) => {
+        if (!date) return true;
+
+        return moment(date).isValid();
+      }),
     gender: z
       .enum([GENDER.MALE, GENDER.FEMALE, GENDER.OTHER])
       .optional()
@@ -32,14 +40,21 @@ export const createUserSchema = z
 export const updateUserSchema = z
   .object({
     firstName: z.string().min(3).optional(),
-    middleName: z.string().min(3).optional(),
-    lastName: z.string().min(3).optional(),
+    middleName: z.string().optional(),
+    lastName: z.string().optional(),
     username: z.string().min(5).optional(),
     email: z.string().email().optional(),
     phoneNumber: z.string().refine(validator.isMobilePhone).optional(),
     password: z.string().min(5).optional(),
     confirmPassword: z.string().min(5).optional(),
-    dob: z.date().optional(),
+    dob: z
+      .string()
+      .optional()
+      .refine((date) => {
+        if (!date) return true;
+
+        return moment(date).isValid();
+      }),
     gender: z.enum([GENDER.MALE, GENDER.FEMALE, GENDER.OTHER]).optional(),
   })
   .superRefine(({ confirmPassword, password }, ctx) => {
